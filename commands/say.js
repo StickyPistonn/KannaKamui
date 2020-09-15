51056c88f4b5dada308cce27d0994841b0086ac7
FaeyzaNursandy <52786210+FaeyzaNursandy@users.noreply.github.com>
2020-09-15 18:29:15 +0700

    Fix Test

--- commands/say.js
+++ commands/say.js
@@ -3,9 +3,9 @@ const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
     if (message.author.id != "593733192905654272") return console.log("This user isn't Bot Owner")
-    let msg = message.content.slice() || null;
+    let msg = message.content.slice();
     const mc = member.guild.channels.cache.find(channels => channel.id === "745487429602902217");
-    mc.send(`${msg}`);
+    mc.send(`${msg.slice()}`);
 }
 
 module.exports.config = {
