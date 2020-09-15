613c5a4290289d7f7e57f2c1c50ec849a3841426
FaeyzaNursandy <52786210+FaeyzaNursandy@users.noreply.github.com>
2020-09-15 17:56:57 +0700

    Update Test

---
+++ commands/say.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js")
+const botconfig = require("../botsettings.json");
+
+module.exports.run = async (bot, message, args) => {
+    if (message.author.id != "593733192905654272") return console.log("This user isn't Bot Owner")
+    let msg = message.content.slice() || null;
+    const mc = member.guild.channels.cache.find(channels => channel.id === "745487429602902217");
+    mc.send(`${msg}`)
+}
+
+module.exports.config = {
+    name: "say",
+    description: "To say something",
+    usage: "k!say",
+    accessableby: "Bot Owner",
+    aliases: []
+}
