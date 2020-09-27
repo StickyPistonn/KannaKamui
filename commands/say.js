18d4f169d640b283685e31d8f2e29bdcdb535add
FaeyzaNursandy <52786210+FaeyzaNursandy@users.noreply.github.com>
2020-09-27 12:04:32 +0700

    Fix

--- commands/say.js
+++ commands/say.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     if (message.author.id != "404763865847562241") return console.log("This user doesn't have access")
     let msg = message.content || null;
     let Channel = message.guild.channels.cache.find(
-        (ch) => ch.id === "753340152105009163"
+        (ch) => ch.id === "745487429602902217"
     );
     Channel.send(`${msg.slice(6)}`);
 }
--- commands/shutdown.js
+++ commands/shutdown.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
-    if (message.author.id != "593733192905654272") return message.channel.send("I'm sorry, you're not my Sticky Oniichan :sweat_smile:")
+    if (message.author.id != "404763865847562241") return message.channel.send("I'm sorry, you're not my Nez Oneechan :sweat_smile:")
 
         try {
             await message.channel.send("Kanna is preparing to sleep...")
